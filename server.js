58c791268057d059bf54370ca2b36f5f5b063351
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-02-17 16:05:53 -0500

    post request + input validation

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const fs = require('fs');
+const path = require('path');
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
@@ -48,12 +56,60 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+
+    // synchronus, doesn't require callback
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        // null means don't edit existing data, 2 creates whitespace between values
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // send 400 error if data in req.body is incorrect
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(animal);
 });
 
 app.get('/api/animals/:id', (req, res) => {
